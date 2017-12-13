710f278ec7f368b246545f538bad2fed96d49a45
Amrit Sidhu <amrit.sidhu@digital.cabinet-office.gov.uk>
2017-12-13 11:39:33 +0000

    Finish user index, and destroy actions

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,19 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @admin     = users(:michael)
+    @non_admin = users(:archer)
+  end
+
   test "should get new" do
     get signup_path
     assert_response :success
   end
+
+
+
+
 
 end
