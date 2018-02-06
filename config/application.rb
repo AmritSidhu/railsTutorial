b60da1a7557e9476bbb137c79e24718a152b3621
Amrit Sidhu <amrit.sidhu@digital.cabinet-office.gov.uk>
2018-02-06 15:32:27 +0000

    asset pipeline issue

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,9 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+config.serve_static_assets = true
+
+
 module Attempt
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
